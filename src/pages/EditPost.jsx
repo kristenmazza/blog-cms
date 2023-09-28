af7cd734076a31d3cf33a96f0c076b70712743e4
kristenmazza <kbmazza@gmail.com>
2023-09-27 19:46:53 -0700

    Set up WYSIWYG editor

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,9 +1,11 @@
 import { Container, Toolbar } from '@mui/material';
+import Tiptap from '../components/TipTap';
 
 export default function EditPost() {
   return (
     <Container maxWidth='lg'>
       <Toolbar sx={{ height: '8rem' }} />
+      <Tiptap />
     </Container>
   );
 }
